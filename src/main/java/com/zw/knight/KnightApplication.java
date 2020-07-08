e2a9145e34cf488ce556c6c87f2ea49604ce92a0
zhaowei <zhaowei@zhangyue.com>
2020-07-08 16:27:46 +0800

    init commint

---
+++ src/main/java/com/zw/knight/KnightApplication.java
@@ -0,0 +1,13 @@
+package com.zw.knight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnightApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zw/knight/KnightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zw.knight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
