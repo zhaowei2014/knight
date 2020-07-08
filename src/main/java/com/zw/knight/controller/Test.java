467e153acf4a9aa0b9555f26a62510a998df808b
zhaowei <zhaowei@zhangyue.com>
2020-07-08 16:52:44 +0800

    调整方式

---
+++ src/main/java/com/zw/knight/controller/Test.java
@@ -0,0 +1,17 @@
+package com.zw.knight.controller;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * testController
+ *
+ * @author zw
+ * @date 2020/7/8
+ */
+@Validated
+@RestController
+@RequestMapping("/address")
+public class Test {
+}
