1d43181379f1b4672f661427214477d820975a0a
zhaowei <zhaowei@zhangyue.com>
2020-07-16 18:50:43 +0800

    调整

--- src/main/java/com/zw/knight/service/Sort.java
+++ src/main/java/com/zw/knight/service/Sort.java
@@ -1,6 +1,7 @@
 package com.zw.knight.service;
 
-import com.zw.knight.util.GsonUtils;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * 排序
@@ -10,20 +11,43 @@ import com.zw.knight.util.GsonUtils;
  */
 public class Sort {
     Integer[] nums = {1};
-    // 二分排序
 
-    // 归并排序
-    public void mergeSort(int[] nums){
+    // 二分排序
+    public void doubleSort(int[] nums, int l, int r) {
+        if (l >= r) return;
+
 
     }
 
+    // 归并排序
+    public void mergeSort(int[] nums, int start, int end) {
+        int[] result = new int[nums.length];
+        if (start >= end) return;
+        int len = end - start;
+        int mid = (len >> 1) + start;
+        int start1 = start;
+        int end1 = mid;
+        int start2 = mid + 1;
+        int end2 = end;
+        mergeSort(nums, start1, end1);
+        mergeSort(nums, start2, end2);
+        int k = start;
+        while (start1 <= end1 && start2 <= end2)
+            result[k++] = nums[start1] > nums[start2] ? nums[start1++] : nums[start2++];
+        while (start1 <= end1)
+            result[k++] = nums[start1++];
+        while (start2 <= end2)
+            result[k++] = nums[start2++];
+        for (int i = 0; i <= end; i++) {
+            nums[i] = result[i];
+        }
+    }
+
     // 插入排序
-    public int[] inSort(int[] nums) {
-        long st = System.nanoTime();
-        int temp;
+    public void inSort(int[] nums) {
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] < nums[i - 1]) {
-                temp = nums[i];
+                int temp = nums[i];
                 for (int j = i; j >= 0; j--) {
                     if (j > 0 && temp < nums[j - 1]) {
                         nums[j] = nums[j - 1];
@@ -34,8 +58,6 @@ public class Sort {
                 }
             }
         }
-        System.out.println("耗时: " + (System.nanoTime() - st));
-        return nums;
     }
 
     // 堆排序
@@ -60,27 +82,42 @@ public class Sort {
         }
     }
 
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
         int[] nums = {9, 8, 7, 6, 5, 4, 3, 2, 1};
-        int[] nums1 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] nums1 = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        int[] result = new int[nums.length];
         Sort sort = new Sort();
 
         // 插入排序
-//        System.out.println(GsonUtils.toJson(sort.inSort(nums)));
-//        System.out.println(GsonUtils.toJson(sort.inSort(nums1)));
-//
+        sort.inSort(nums);
+        sort.print(sort, "inSort");
 //        // 快速排序
 //        long st = System.nanoTime();
 //        sort.quickSort(nums1, 0, nums.length - 1);
-//        System.out.println("quick耗时: " + (System.nanoTime() - st));
-//        System.out.println(GsonUtils.toJson(nums));
 //        long st1 = System.nanoTime();
 //        sort.quickSort(nums1, 0, nums1.length - 1);
+//        System.out.println("quick耗时: " + (st1 - st));
 //        System.out.println("quick耗时: " + (System.nanoTime() - st1));
-//        System.out.println(GsonUtils.toJson(nums1));
+//
+//        // 归并排序
+//        long st3 = System.nanoTime();
+//        sort.mergeSort(nums, result, 0, nums.length - 1);
+//        long st4 = System.nanoTime();
+//        sort.mergeSort(nums1, result, 0, nums1.length - 1);
+//        System.out.println("merge耗时: " + (st4 - st3));
+//        System.out.println("merge耗时: " + (System.nanoTime() - st4));
+    }
 
-        // 归并排序
-        System.out.println(50 >> 1);
+    private void print(Object obj, String name) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        int[] nums = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        Method method = Sort.class.getMethod(name, Sort.class);
+        Sort sort = new Sort();
+        Method[] methods = Sort.class.getMethods();
+        for (Method method1 : methods) {
+            System.out.println(method1.getName());
+        }
+//        long st = System.nanoTime();
+//        method.invoke(obj, nums, 0, nums.length);
+//        System.out.println(name + "耗时:" + (System.nanoTime() - st));
     }
 }
